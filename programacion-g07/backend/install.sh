f3c66ca4356fc543ad34589c910cf5bbfac0ae15
Ignacio <90201295+Ignacio-Gutierrez@users.noreply.github.com>
2023-03-23 11:10:07 -0300

    Update install.sh

--- programacion-g07/backend/install.sh
+++ programacion-g07/backend/install.sh
@@ -1,9 +1,3 @@
 python3 -m venv .
 source bin/activate
-<<<<<<< HEAD
 pip install -r requirements.txt
-=======
-pip3 install -r requirements.txt
-
-#sudo chmod +x install.sh
->>>>>>> ca742e7 (clase01.a)
