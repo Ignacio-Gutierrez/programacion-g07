362b95c426edd3e45fb8142e513a79eaf18dc74d
Ignacio <90201295+Ignacio-Gutierrez@users.noreply.github.com>
2023-03-23 11:05:27 -0300

    Delete clase 01-02 directory

--- clase 01-02/backend/boot.sh
+++
@@ -1,7 +0,0 @@
-source bin/activate
-python3 app.py
-
-
-#sudo chmod +x boot.sh
-
-#./boot.sh
--- clase 01-02/backend/install.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv .
-source bin/activate
-pip3 install -r requirements.txt
-
-#sudo chmod +x install.sh
