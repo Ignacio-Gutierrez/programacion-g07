20c78dd77a6855ec1f7baca786faf67d9ca62ab0
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-10-12 14:03:39 -0300

    paginacion complete

--- frontend/app_gym/src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ frontend/app_gym/src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -12,7 +12,6 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
 export class ListaUsuariosComponent {
   arrayUsers: any;
   currentPage: number = 1;
-  maxPages: number = 3;
 
   constructor(
     private router: Router,
@@ -35,10 +34,19 @@ export class ListaUsuariosComponent {
   }
 
   cargarPaginaSiguiente() {
-    if (this.currentPage > 1) {
-      this.currentPage++;
-      this.cargarUsuarios();
-    }
+    const nextPage = this.currentPage + 1;
+  
+    // Call the usuariosService to fetch data for the next page
+    this.usuariosService.getUsers(nextPage).subscribe((data: any) => {
+      if (data.usuarios && data.usuarios.length > 0) {
+        // If there are users on the next page, update the currentPage and load the data
+        this.currentPage = nextPage;
+        this.cargarUsuarios();
+      } else {
+        // If there are no users on the next page, you can choose to handle it (e.g., display a message)
+        console.log('No users on the next page.');
+      }
+    });
   }
   
   cargarPaginaPrevia() {
