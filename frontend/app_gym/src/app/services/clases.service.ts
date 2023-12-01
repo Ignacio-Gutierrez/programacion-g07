0b3ecfd6d8499698542838ee43dd0846445bb99d
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-12-01 00:27:47 -0300

    services clases

--- frontend/app_gym/src/app/services/clases.service.ts
+++ frontend/app_gym/src/app/services/clases.service.ts
@@ -16,4 +16,41 @@ export class ClasesService {
     });
       return this.httpClient.get(this.url + '/clases', {headers: headers});
   }
+
+  getClase(claseID: number){
+    let auth_token=localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`   
+    });
+      return this.httpClient.get(this.url + '/clase/' + claseID, {headers: headers});
+  }
+   
+  createClase(claseData: any) {
+    const auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+    return this.httpClient.post(this.url + '/clases', claseData, { headers: headers });
+  }
+
+  updateClase(id: number, claseData: any) {
+    const auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+    return this.httpClient.put(this.url + '/clase/' + id, claseData, { headers: headers });
+  }
+  
+  deleteClase(id: number) {
+    const auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${auth_token}`
+    });
+    return this.httpClient.delete(this.url + '/clase/' + id, { headers: headers });
+  }
 }
+
