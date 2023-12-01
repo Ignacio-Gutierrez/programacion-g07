cbf0e6db036e29cf6bfe0f0ea70c8e1fefbd8a5e
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-12-01 19:45:42 -0300

    new component clase

--- frontend/app_gym/src/app/app.module.ts
+++ frontend/app_gym/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { VistaInicioComponent } from './pages/vista-inicio/vista-inicio.componen
 import { VistaErrorComponent } from './pages/vista-error/vista-error.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClaseComponent } from './components/clase/clase.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     VistaInicioComponent,
     ListaUsuariosComponent,
     VistaErrorComponent,
+    ClaseComponent,
 
   ],
   imports: [
